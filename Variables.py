5fd03d0ce3316735a1b09c393daab156f9d60c28
Jashwin P <jashwinpakala@gmail.com>
2025-09-20 18:26:11 +0530

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,11 @@
+#Creating different variables
+x=5
+y="John"
+print(x)
+print(y)
+codingal="codingal"
+print(codingal)
+#Userinput
+name=input("enter your name:")
+print("\nHello", name, "\nWelcome to codingal")
+
